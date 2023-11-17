d2951faeeb5049fb573b30219bc6bf9c9b84b53b
Filza01 <filza.shahid@10xengineers.ai>
2023-11-17 11:39:41 +0500

    env for axi4-Lite interface, not tested yet

--- gpio/verif/read_agent/rd_sequencer.sv
+++
@@ -1,9 +0,0 @@
-class rd_sequencer extends uvm_sequencer #(axi_seq_item);
-  `uvm_component_utils(rd_sequencer )// factory registeration
-
-     // Constructor - required syntax for UVM automation and utilities
-  function new (string name, uvm_component parent);
-    super.new(name, parent);
-  endfunction : new
-
-endclass //rd_sequencer extends uvm_sequencer
--- gpio/verif/write_agent/wr_sequencer.sv
+++
@@ -1,9 +0,0 @@
-class wr_sequencer extends uvm_sequencer #(axi_seq_item);
-    `uvm_component_utils(wr_sequencer)
-
-    function new(string name = "wr_sequencer", uvm_component parent);
-       super.new(name, parent);
-    endfunction
- 
- endclass
- 
